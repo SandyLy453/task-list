1b075737156774823c14090bc9e1185b93346317
Boi San Ly <144407141+SandyLy453@users.noreply.github.com>
2024-10-20 21:55:27 -0700

    Update Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Task({ task, toggleComplete, removeTask }) {
+export default function Task({ name, checked, removeTask, toggleTaskStatus }) {
+  useEffect(() => {}, [checked]);
+
   return (
     <>
-      <div className={`task ${task.completed ? 'completed' : ''}`}>
+      <div className="task">
         <label>
           <input
-            id="checkbox"
             type="checkbox"
-            checked={task.completed}
-            onChange={toggleComplete}
+            className="checkBox"
+            checked={checked}
+            onChange={() => toggleTaskStatus(name)}
           />
         </label>
-        <span className="task-name" style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.name}</span>
-        <button className="task-delete" onClick={removeTask}>Remove</button>
+        <p
+          className="content"
+          style={{ textDecoration: checked ? "line-through" : "none" }} 
+        >
+          {name}
+        </p>
+        <button className="task-delete" onClick={() => removeTask(name)}>
+          Remove
+        </button>
       </div>
     </>
   );
